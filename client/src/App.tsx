505266d9aa71296ae5f4f0bac01f45223e99f9be
John Marquez <johnmarquez249@gmail.com>
2024-12-09 22:17:22 -0700

    Fixed Render issue to login

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,25 @@
-
 import './App.css';
 import { Outlet } from 'react-router-dom';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import Auth from './utils/auth'; // Assuming this has methods to get the token.
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3001/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = Auth.loggedIn() ? Auth.getToken() : null; // Check if user is logged in and get the token.
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '', // Attach the token to the headers.
+    },
+  };
+});
 
 const client = new ApolloClient({
-  uri: 'http://localhost:3001/graphql',
+  link: authLink.concat(httpLink), // Combine the auth link with the HTTP link.
   cache: new InMemoryCache(),
 });
 
